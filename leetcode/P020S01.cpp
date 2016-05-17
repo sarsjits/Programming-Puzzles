ebb0895e9b5b4609c4eb516547f031cb474d86a3
sarsjits <sarsjits@gmail.com>
2016-05-17 22:54:12 +0530

    Leetcode: Valid Parantheses (Solution 01)

---
+++ leetcode/P020S01.cpp
@@ -0,0 +1,24 @@
+#include <stack>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+                st.push(s[i]);
+            else if(s[i] == ')' && st.size() != 0 && st.top() == '(') {
+                st.pop();
+            } else if(s[i] == '}' && st.size() != 0 && st.top() == '{') {
+                st.pop();
+            } else if(s[i] == ']' && st.size() != 0 && st.top() == '[') {
+                st.pop();
+            } else {
+                return false;
+            }
+        }
+        if(st.size() != 0)
+            return false;
+        else
+            return true;
+    }
+};
