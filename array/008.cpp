416b248ca1e7562dd62d81c359a121fa6519490b
sarsjits <sarsjits@gmail.com>
2016-08-17 15:44:29 +0530

    Update: Median of two sorted arrays of size n

--- array/008.cpp
+++ array/008.cpp
@@ -1,8 +1,57 @@
-// Median of two sorted arrays (same size) in O(log n) using Divide and Conquer
+/* 
+	> Median of two sorted arrays of size n
+	> Since the total number of elements would be even, median would be avg of two
+	URL: http://bit.ly/17zECdh
+*/
 
 #include <iostream>
 
 using namespace std;
+
+/*
+	Method 1: Simple counting while merging
+	Time: O(1)
+	Space: O(1)
+*/
+
+int getMedianLinear(int *a,int *b,int n) {
+	int i = 0; /* Count for a */
+	int j = 0; /* Count for b */
+	int count = 0; /* Count for total */
+	int med1 = -1, med2 = -1;
+	
+	for (count = 0; count <= n; count += 1) {
+		/*
+			Case where all the elements in the array 1 are smaller than in array 2
+		*/
+		if(i == n) {
+			med1 = med2;
+			med2 = b[0];
+			break;
+		} else if(j == n) {
+			med1 = med2;
+			med2 = a[0];
+			break;
+		}
+		
+		if(a[i] < b[i]) {
+			med1 = med2;
+			med2 = a[i];
+			i++;
+		} else {
+			med1 = med2;
+			med2 = b[j];
+			j++;
+		}
+	}
+	return (med1 + med2)/2;
+}
+
+/*
+	Method 2: Using Divide and Conquer
+	Time: O(logn)
+	Space: 
+*/
 
 // Returns median of the list of numbers
 int median(int *a,int n) {
@@ -39,7 +88,7 @@ int getMedian(int *a,int *b,int n) {
 int main() {
 	int m[] = {1,2,3,6};
 	int n[] = {4,6,8,10};
-	cout << getMedian(m,n,4);
+	cout << getMedianLinear(m,n,4);
 	cout << "\n";
 	return 0;
 }
