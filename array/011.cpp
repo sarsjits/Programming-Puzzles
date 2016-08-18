c5190eb5ec106fd58355a4060c1efbddca0dd944
sarsjits <sarsjits@gmail.com>
2016-08-18 23:07:10 +0530

    Update: Maximum sum such that no two elements are adjacent.

--- array/011.cpp
+++ array/011.cpp
@@ -1,10 +1,18 @@
-// Maximum sum such that no two elements are adjacent
+/* 
+	Maximum sum such that no two elements are adjacent
+	URL: http://bit.ly/2bAurt3
+*/
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+/*
+	Method 1: Does not work for the negative numbers
+	Time: O(n)
+	Space: O(1)
+*/
 int maxSum(vector<int> &a) {
 	int incl = a[0]; // Maximum sum including prev element for idx 1
 	int excl = 0; // MAximum sum excluding prev element for idx 1
@@ -19,8 +27,29 @@ int maxSum(vector<int> &a) {
 	return max(incl,excl);
 }
 
+/*
+	Method 2: Using Dynamic Programming
+	Simple yet useful explanation at URL: http://bit.ly/2b3R2yr
+	Read answer by codaddict
+	Time: O(n)
+	Space: O(n)
+*/
+
+int maxSumDP(vector<int> &a) {
+	int sum[a.size()];
+	for(int i=0;i<a.size();++i) {
+		if(i == 0)
+			sum[0] = a[0];
+		else if(i == 1)
+			sum[1] = max(a[1],sum[0]);
+		else
+			sum[i] = max(sum[i-2]+a[i],sum[i-1]);
+	}
+	return sum[a.size()-1];
+}
+
 int main() {
 	vector<int> a = {5,5,10,40,50,35};
-	cout << maxSum(a) << "\n";
+	cout << maxSumDP(a) << "\n";
 	return 0;
 }
